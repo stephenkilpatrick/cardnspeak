fc586c38e6315dc9aff8afbf2795f58765e77ba5
stephenkilpatrick <skk@skk.blue>
2019-02-07 23:36:05 +0000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,4 +2,5 @@
 sudo apt-get update
 sudo apt-get dist-upgrade
 sudo apt-get install libttspico-utils
-pico2wave -w ~/startupcomplete.wav "Startup is complete."
+pico2wave -w ~/startupcomplete.wav "Startup is complete"
+pico2wave -w ~/noaudiofile.wav "No matching audio file found"
