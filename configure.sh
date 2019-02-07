cb906a1970ba2aacb488484a53930fdd913d235b
stephenkilpatrick <skk@skk.blue>
2019-02-07 23:39:23 +0000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 sudo apt-get update
 sudo apt-get dist-upgrade
 sudo apt-get install libttspico-utils
 pico2wave -w ~/startupcomplete.wav "Startup is complete"
 pico2wave -w ~/noaudiofile.wav "No matching audio file found"
+echo ~/.bashrc >> aplay ~/startupcomplete.wav
+echo ~/.bashrc >> bash ~/cardnspeak.sh
