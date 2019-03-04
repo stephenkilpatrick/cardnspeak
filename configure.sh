b10c427212692dbaab8ad3aa4ba212f0a16dc185
stephenkilpatrick <skk@skk.blue>
2019-03-04 21:24:58 +0000

    Update configure.sh
    Added newer version of usbmount than is available in the regular repo. Thanks to AlbertB's super helpful post on the Raspberry Pi forums for to work around for something that seems needlessly broken. https://www.raspberrypi.org/forums/viewtopic.php?t=192291

--- configure.sh
+++ configure.sh
@@ -2,7 +2,12 @@
 
 sudo apt-get update
 sudo apt-get -y dist-upgrade
-sudo apt-get install -y libttspico-utils
+sudo apt-get install -y libttspico-utils debhelper
+git clone https://github.com/rbrito/usbmount.git
+cd usbmount
+sudo dpkg-buildpackage -us -uc -b
+cd ~
+sudo apt install ./usbmount_0.0.24_all.deb
 pico2wave -w ~/startupcomplete.wav "Startup is complete"
 pico2wave -w ~/noaudiofile.wav "No matching audio file found"
 pico2wave -w ~/copyingfiles.wav "Copying files from USB drive"
