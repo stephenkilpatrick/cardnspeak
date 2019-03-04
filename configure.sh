969bd8608e0ecf203d9edc76223f910589f35ad8
stephenkilpatrick <skk@skk.blue>
2019-03-04 22:07:26 +0000

    Update configure.sh
    Moved bashrc stuff to its own file. Easier to manage and I don't know what I'm doing with echo anyway.

--- configure.sh
+++ configure.sh
@@ -12,12 +12,5 @@ pico2wave -w ~/startupcomplete.wav "Startup is complete"
 pico2wave -w ~/noaudiofile.wav "No matching audio file found"
 pico2wave -w ~/copyingfiles.wav "Copying files from USB drive"
 pico2wave -w ~/copyingcomplete.wav "Copying complete."
-echo aplay ~/startupcomplete.wav >> ~/.bashrc
-echo bash ~/cardnspeak.sh >> ~/.bashrc
-echo "if [ "\$(ls -A /mnt/usb/*.[mM][pP]3)\" ];then
-  aplay ~/copyingfiles.wav
-  cp /media/usb/*.[mM][pP]3 ~
-  rename 'y/A-Z/a-z/' ~/*.*
-  aplay ~/copyingcomplete.wav
-fi" >> bash.rc
+cat bashrc ~/.bashrc
 cp cardnspeak.sh ~
