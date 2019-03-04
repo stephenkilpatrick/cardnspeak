e33770a374fa41694d2f3ef7e0b8110be9fe292c
stephenkilpatrick <skk@skk.blue>
2019-03-04 21:14:22 +0000

    Update configure.sh
    Fixed syntax issue on echos. Added -y flags to the apt commands.

--- configure.sh
+++ configure.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get dist-upgrade
-sudo apt-get install libttspico-utils
+sudo apt-get -y dist-upgrade
+sudo apt-get install -y libttspico-utils
 pico2wave -w ~/startupcomplete.wav "Startup is complete"
 pico2wave -w ~/noaudiofile.wav "No matching audio file found"
-echo ~/.bashrc >> aplay ~/startupcomplete.wav
-echo ~/.bashrc >> bash ~/cardnspeak.sh
+pico2wave -w ~/copyingfiles.wav "Copying files from USB drive"
+pico2wave -w ~copyingcomplete.wav "Copying complete."
+echo aplay ~/startupcomplete.wav >> ~/.bashrc
+echo bash ~/cardnspeak.sh >> ~/.bashrc
 cp cardnspeak.sh ~
