f6079d8071d6f4a201bf69f2244a04b97f76fc30
stephenkilpatrick <skk@skk.blue>
2019-03-04 22:37:39 +0000

    Update configure.sh
    Changed dots to $PWD. apt seemed to hate that for some reason.

--- configure.sh
+++ configure.sh
@@ -7,10 +7,10 @@ git clone https://github.com/rbrito/usbmount.git
 cd usbmount
 sudo dpkg-buildpackage -us -uc -b
 cd ~
-sudo apt install ./usbmount_0.0.24_all.deb
+sudo apt install -y $PWD/usbmount_0.0.24_all.deb
 pico2wave -w ~/startupcomplete.wav "Startup is complete"
 pico2wave -w ~/noaudiofile.wav "No matching audio file found"
 pico2wave -w ~/copyingfiles.wav "Copying files from USB drive"
 pico2wave -w ~/copyingcomplete.wav "Copying complete."
-cat ./bashrc >> ~/.bashrc
+cat $PWD/bashrc >> ~/.bashrc
 cp cardnspeak.sh ~
