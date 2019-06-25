afe65fd903c64141a08a55c701ece0619269f408
stephenkilpatrick <skk@skk.blue>
2019-06-25 13:23:07 +0000

    Workaround for buster
    Removed download of libtts until it is re-added into the buster repo. Commented out the pico2wave lines until text-to-speech package is restored to buster, or a suitable replacement is found.

--- configure.sh
+++ configure.sh
@@ -2,15 +2,16 @@
 
 sudo apt-get update
 sudo apt-get -y dist-upgrade
-sudo apt-get install -y libttspico-utils debhelper mpg123
+sudo apt-get install -y debhelper mpg123
 git clone https://github.com/rbrito/usbmount.git
 cd usbmount
 sudo dpkg-buildpackage -us -uc -b
 cd ..
 sudo apt install -y $PWD/usbmount_0.0.24_all.deb
-pico2wave -w ~/startupcomplete.wav "Startup is complete"
-pico2wave -w ~/noaudiofile.wav "No matching audio file found"
-pico2wave -w ~/copyingfiles.wav "Copying files from USB drive"
-pico2wave -w ~/copyingcomplete.wav "Copying complete."
+# pico2wave -w ~/startupcomplete.wav "Startup is complete"
+# pico2wave -w ~/noaudiofile.wav "No matching audio file found"
+# pico2wave -w ~/copyingfiles.wav "Copying files from USB drive"
+# pico2wave -w ~/copyingcomplete.wav "Copying complete."
 cat $PWD/bashrc >> ~/.bashrc
-cp cardnspeak.sh ~
+mv cardnspeak.sh ~
+mv *.mp3 ~
